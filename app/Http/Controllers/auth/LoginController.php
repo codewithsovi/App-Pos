50465ff55937c5a96ec952922a842a3284dfa0c9
sovi <sovialfina@gmail.com>
2025-06-29 21:42:46 +0700

    first commit

---
+++ app/Http/Controllers/auth/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+       $credentials = $request->validate(
+            [
+                'email' => 'required|email',
+                'password' => 'required',
+            ],
+            [
+                'email.required' => 'email harus diisi',
+                'email.email' => 'email tidak valid',
+                'password.required' => 'password harus diisi'
+            ],
+        );
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('dashboard'));
+        }
+
+        return back()->withErrors([
+            'email' => 'kredential yang anda masukkan tidak sesuai dengan data kami',
+        ])->onlyInput('email');
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/dahboard', [DashboardController::class, 'index'])->name('dashboard');
